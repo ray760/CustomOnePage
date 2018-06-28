0a34fa990a44b183b9be16a7afc353d014d8a644
Ray Robertson <ray760@yahoo.com>
2018-06-28 18:18:37 -0400

    Several updates. Last update is getting modal to work from child component to parent.

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,50 @@ import '../css/app.scss';
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
+import YTVideo from './components/ytVideo'
 import IntroDiv from './components/intro';
-/*ReactDOM.render(
-  <section class="top-section site-section"></section>,
-  document.getElementById('root')
-);*/
+import AboutDiv from './components/about-me';
+import Auth0Div from './components/about-auth0';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      centered: true,
+      modal: false,
+      size: 'lg'
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
   render() {
 
     return (
       <div>
+        <Modal centered={this.state.centered} size={this.state.size} isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+          <ModalBody>
+            <YTVideo />
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
         <IntroDiv />
+        <AboutDiv modal={this.toggle} />
+        <Auth0Div />
       </div>
     )
 
--- src/js/components/intro.js
+++ src/js/components/intro.js
@@ -3,7 +3,14 @@ import React from 'react';
 const introDiv = () => {
   return (
     <section className="top-section site-section">
-      Intro
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <h1>Hi, I'm <span className="heavy">Ray Robertson</span></h1>
+          <p className="col-sm-6 col-md-12">
+            If you're here right now then more than likely you're an Auth0 employee, and that's awesome because I'd like to be one too. So here's my simple pitch!
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
